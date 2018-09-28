5d5408080ef1d6b477fa271f0a0c96d0fb8943ca
stephan-dev <stephan-dev@outlook.fr>
2018-09-28 04:21:42 +0200

    mofo website becomes dynamic, updated read me

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# stephan-dev.github.io
+# BetterMotherFuckingWebsite @stephan-dev.github.io
+A Mofo demo website for the hacking project
 
-BetterMotherFuckingWebsite en production : https://stephan-dev.github.io/
+## features
+* some CSS
+* very oldskool popups
+* rick astley
+
+## url
+ production : https://stephan-dev.github.io/
---
+++ dynamic.js
@@ -0,0 +1,13 @@
+ function popup1() {
+  	window.alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+    }
+
+  function popup2() {
+  	window.confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")
+  	if(window.confirm) {
+  		window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  		}
+    }
+
+  setTimeout(popup1, 10000);
+  setTimeout(popup2, 20000);
